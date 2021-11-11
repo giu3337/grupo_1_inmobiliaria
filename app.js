ac11f1fbd2a8bd3daa8954fde87159d56a9d8a09
juancmayac <jcmayac@hotmail.com>
2021-11-11 17:01:16 -0500

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
  * 
  */
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 /**
@@ -12,8 +14,10 @@ app.listen(3000, () => console.log('Servidor corriendo.'));
 /**
  * 
  */
-app.get('/', (req, res) => res.send('Raiz') );
+//app.get('/', (req, res) => res.send('Raiz') );
 app.get('/Inicio', (req, res) => res.send('Inicio') );
 app.get('/Renta', (req, res) => res.send('Renta') );
 app.get('/Venta', (req, res) => res.send('Venta') );
 app.get('/Contacto', (req, res) => res.send('Contacto') );
+
+app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ) );
