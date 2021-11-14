d346220474d91b93f773443932b3082dd37a0987
juancmayac <jcmayac@hotmail.com>
2021-11-14 10:30:56 -0500

    Readme

--- app.js
+++ app.js
@@ -14,10 +14,9 @@ app.listen(3000, () => console.log('Servidor corriendo.'));
 /**
  * 
  */
-//app.get('/', (req, res) => res.send('Raiz') );
-app.get('/Inicio', (req, res) => res.send('Inicio') );
+app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ) );
 app.get('/Renta', (req, res) => res.send('Renta') );
 app.get('/Venta', (req, res) => res.send('Venta') );
 app.get('/Contacto', (req, res) => res.send('Contacto') );
+app.get('/404', (req, res) => res.send('"Error página no encontrada"') );
 
-app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ) );
