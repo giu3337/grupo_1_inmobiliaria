fd6fafeaabe361afe2aff576dbbcaf6701e29273
juancmayac <jcmayac@hotmail.com>
2021-11-23 22:59:59 -0500

    Pages

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.listen(3000, () => console.log('Servidor corriendo.'));
  * 
  */
 app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ) );
-app.get('/Renta', (req, res) => res.send('Renta') );
-app.get('/Venta', (req, res) => res.send('Venta') );
-app.get('/Contacto', (req, res) => res.send('Contacto') );
-app.get('/404', (req, res) => res.send('"Error página no encontrada"') );
 
+app.get('/Renta', (req, res) => res.sendFile(path.resolve(__dirname, './views/renta.html') ) );
+app.get('/Venta', (req, res) => res.sendFile(path.resolve(__dirname, './views/venta.html') ) );
+app.get('/Contacto', (req, res) => res.sendFile(path.resolve(__dirname, './views/contacto.html') ) );
+app.get('/404', (req, res) => res.sendFile(path.resolve(__dirname, './views/404.html') ) );
