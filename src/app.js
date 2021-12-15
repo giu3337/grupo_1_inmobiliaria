fac4edee9041f5a2119265a88f16d97760e71dee
aleeeexis <alexis.vazquez.ml@gmail.com>
2021-12-14 23:08:44 -0300

    arrancando sprint 2

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
-/**
- * 
- */
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-/**
- * 
- */
-app.listen(3000, () => console.log('Servidor corriendo.'));
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => console.log('Servidor en http://localhost:'+app.get('port')))
+app.use(express.static(path.resolve(__dirname, '..','public')))
 
-/**
- * 
- */
-app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ) );
-
-app.get('/Renta', (req, res) => res.sendFile(path.resolve(__dirname, './views/renta.html') ) );
-app.get('/Venta', (req, res) => res.sendFile(path.resolve(__dirname, './views/venta.html') ) );
-app.get('/Contacto', (req, res) => res.sendFile(path.resolve(__dirname, './views/contacto.html') ) );
-app.get('/404', (req, res) => res.sendFile(path.resolve(__dirname, './views/404.html') ) );
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views",'index.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,"./views",'login.html')))
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views",'register.html')))
+app.get('/product', (req,res) => res.sendFile(path.resolve(__dirname,"./views",'product.html')))
+app.get('/cart', (req,res) => res.sendFile(path.resolve(__dirname,"./views",'cart.html')))
